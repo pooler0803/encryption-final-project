f1400d2298bf7a66f08b0e2b9ce07ad50e35e1ff
pooler0803 <raven.roberts@saintleo.edu>
2025-10-19 19:58:31 -0400

    Create aes_encryption.py

---
+++ aes_encryption.py
@@ -0,0 +1,52 @@
+from cryptography.hazmat.primitives import hashes, padding
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+import os
+
+def derive_key(password, salt):
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+        backend=default_backend()
+    )
+    return kdf.derive(password.encode())
+
+def encrypt_text(plaintext, password):
+    salt = os.urandom(16)
+    iv = os.urandom(16)
+    key = derive_key(password, salt)
+
+    padder = padding.PKCS7(128).padder()
+    padded = padder.update(plaintext.encode()) + padder.finalize()
+
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+    encryptor = cipher.encryptor()
+    ct = encryptor.update(padded) + encryptor.finalize()
+
+    return salt + iv + ct
+
+def decrypt_text(blob, password):
+    salt, iv, ct = blob[:16], blob[16:32], blob[32:]
+    key = derive_key(password, salt)
+
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+    decryptor = cipher.decryptor()
+    padded = decryptor.update(ct) + decryptor.finalize()
+
+    unpadder = padding.PKCS7(128).unpadder()
+    data = unpadder.update(padded) + unpadder.finalize()
+
+    return data.decode()
+
+# ---- Example Execution ----
+if __name__ == "__main__":
+    password = "StrongPass123"
+    text = "ExamplePassword123!"
+    encrypted = encrypt_text(text, password)
+    decrypted = decrypt_text(encrypted, password)
+
+    print("AES Encrypted (hex):", encrypted.hex())
+    print("Decrypted:", decrypted)
